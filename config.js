99c4a60d5e8985f4262653df2545cfc494602beb
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-15 20:55:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+global.owner = ['62895361677059','6281567956300'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = ['62895361677059'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'Kokoronationz'
+}
+
+// Sticker WM
+global.packname = 'Kuriyama-bot'
+global.author = '@Kokoronationz'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
