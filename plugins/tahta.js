faf8a80595dbb53323a0854bc1179fb2a75993e3
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-15 21:49:27 +0700

    update creator

--- plugins/tahta.js
+++ plugins/tahta.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text }) => {
   }
 }
 handler.help = ['tahta <teks>']
-handler.tags = ['nulis']
+handler.tags = ['creator']
 handler.command = /^((harta)?tahta)$/i
 handler.group = true
 handler.register = true
