b68a0bac0185b0211ad099a6235cf085a4f3d288
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-16 23:22:51 +0700

    update photooxy.js

--- plugins/photooxy.js
+++ plugins/photooxy.js
@@ -1,90 +1,17 @@
 let fetch = require('node-fetch')
-
-let handler = async(m, { conn, text }) => {
-let [effect, teks] = text.split `|`
-
-let listeffect = `
-*List Effect Photooxy :*
-
-smoke
-wolfmetal
-underwaterocean
-typography
-neonlight
-google
-rainbowshine
-camuflage
-3dglowing
-vintage
-candy
-gradientavatar
-glowrainbow
-stars
-fur
-flaming
-crispchrome
-kueultah
-rainbowbg
-metalicglow
-striking3d
-watermelon
-underwebmatrix
-multimaterial
-harrypotter
-8bit
-kopi2
-luxuryroyal
-gerbang
-woodblock
-smoketypography
-sweetcandy
-silk
-bevel
-partyneon
-greenleaves
-modernmetal
-lolcover
-warface
-pentakill
-aov
-battlefield
-avatarlol
-pokemon
-lolavatarglitch
-shinebannerlol
-mastery7lol
-dota2avatar
-lol
-crossfire
-glowpentakill
-warfacecover
-coveroverwatch
-lolcover2
-csgo
-lolpentakill
-`.trim()
-
-    if (!effect) return conn.reply(m.chat, listeffect, m)
-    if (!teks) return conn.reply(m.chat, 'Uhm... Teksnya?', m)
-
-  await m.reply('Sedang membuat...')
- let hasil = await (await fetch('https://api.xteam.xyz/photooxy/' + effect + '?text=' + teks + '&APIKEY=APIKEYMU')).buffer()
- let caption = `*PHOTOOXY*\n\nEffect : ${effect}`
-
-    conn.sendFile(m.chat, hasil, 'photooxy.jpg', caption, m)
+let handler = async (m, { conn, args: [effect], text: txt }) => {
+  let { effects } = await (await (fetch(global.API('xteam', '/photooxy')))).json()
+  if (!effect) throw '*List Effect*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  effect = effect.toLowerCase()
+  if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
+  let url = global.API('xteam', '/photooxy/' + effect, text, 'APIKEY')
+  await conn.sendFile(m.chat, url, 'textpro.jpg', `*TEXTPRO*\n*Effect:* ${effect}`, m)
 }
-handler.help = ['photooxy <effect|teks>']
+handler.help = ['photooxy'].map(v => v + ' <effect> <text>')
 handler.tags = ['creator']
-handler.command = /^(photooxy)$/i
-handler.owner = false
-handler.mods = false
+handler.command = /^(textpro)$/i
 handler.premium = true
-handler.group = false
-handler.private = false
 handler.register = true
 
-handler.fail = null
-handler.exp = 0
-handler.limit = false
-
 module.exports = handler
+
