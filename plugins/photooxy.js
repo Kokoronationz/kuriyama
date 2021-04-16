b10736244a1393e46e855f3aa1e796faa37db8fe
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-16 23:28:31 +0700

    update photooxy.js

--- plugins/photooxy.js
+++ plugins/photooxy.js
@@ -1,13 +1,16 @@
 let fetch = require('node-fetch')
-let [effect, teks] = text.split `|`
-let handler = async (m, { conn, text }) => {
+let split = '|'
+let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/photooxy')))).json()
   if (!effect) throw '*List Effect*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
   if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
-  let url = global.API('xteam', '/photooxy/' + effect, text, 'APIKEY')
-  await conn.sendFile(m.chat, url, 'textpro.jpg', `*TEXTPRO*\n*Effect:* ${effect}`, m)
+  let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
+  text2 = text2.join(split)
+  let url = global.API('xteam', '/photooxy/' + effect, { text, text2 }, 'APIKEY')
+  await conn.sendFile(m.chat, url, 'photooxy.jpg', `*Photooxy*\n*Effect:* ${effect}`, m)
 }
+
 handler.help = ['photooxy'].map(v => v + ' <effect> <text>')
 handler.tags = ['creator']
 handler.command = /^(textpro)$/i
