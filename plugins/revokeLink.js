0c214f3fa7ef1ce2df002e5e649ebc6e5510ecd2
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-16 20:21:06 +0700

    update revokeLink.js

--- plugins/revokeLink.js
+++ plugins/revokeLink.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args }) => {
-  this.fakeReply(m.chat, 'Link group berhasil di setel ulang', '0@s.whatsapp.net', `${conn.user.name} Verified Bot`, m.chat)
+  
   await conn.revokeInvite(m.key.remoteJid)
 }
 handler.help = ['revoke']
