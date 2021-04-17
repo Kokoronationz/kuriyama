f7003191ec2fe567fc2b096e11e61789a6878509
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-17 18:14:04 +0700

    update artimimpi.js

--- plugins/artimimpi.js
+++ plugins/artimimpi.js
@@ -2,6 +2,7 @@
 
 let handler = async(m, { conn, text, usedPrefix }) => {
 
+    await m.reply('Searching...')
     if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'artimimpi ular', m)
 
     axios.get(`https://docs-jojo.herokuapp.com/api/artimimpi?q=` + text)
