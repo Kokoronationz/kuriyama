be5f933ae0f2ba6e95501c9acc5c82ae942d5f11
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-17 15:31:53 +0700

    Add files via upload

---
+++ plugins/chord.js
@@ -0,0 +1,29 @@
+  const axios = require('axios')
+
+let handler = async(m, { conn, text, usedPrefix }) => {
+
+    if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'chord hanya rindu', m)
+
+    axios.get(`https://docs-jojo.herokuapp.com/api/chord?q=` + text)
+        .then((res) => {
+            conn.reply(m.chat, res.data.result, m)
+        })
+        .catch()
+}
+handler.help = ['chord <judul lagu>']
+handler.tags = ['internet']
+handler.command = /^(chord)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
