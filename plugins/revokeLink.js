a68fd4f8f54969df638b832ab574191f1eb166d8
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-17 15:46:01 +0700

    update revokeLink.js

--- plugins/revokeLink.js
+++ plugins/revokeLink.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, args }) => {
   
-  await conn.revokeInvite()
+  await conn.revokeInvite(m.chat)
 }
 handler.help = ['revoke']
-handler.tags = ['']
+handler.tags = ['maintenance']
 handler.command = /^revoke$/i
 handler.owner = false
 handler.mods = false
