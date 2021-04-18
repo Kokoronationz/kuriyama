f250c9f6a97342dfff1d77d3a8df8e5cc68069cb
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-18 23:43:31 +0700

    update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -145,7 +145,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).join`|`})`, 'g'), (_, name) => ''+replace[name])
-    conn.sendFile(m.chat, kuriyama, 'kuriyama.jpg', text.trim(), { key: { remoteJid: '0@s.whatsapp.net', fromMe: false }, message: { conversation: 'Bot by Kokoronationz' }}, m)
+    conn.sendFile(m.chat, kuriyama, 'kuriyama.jpg', text.trim(), { key: { remoteJid: '0@s.whatsapp.net', fromMe: false }, message: { conversation: `${conn.user.name} Verified Bot` }}, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
