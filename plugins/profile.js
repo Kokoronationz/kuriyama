8b1d4ff832b1aa3cf34bbbec547d9e9933f72e46
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-19 06:32:30 +0700

    update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn }) => {
 
   } finally {
     let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-    let { name, uang, limit, exp, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[m.sender]
+    let { name, uang, limit, exp, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[who]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let str = `
@@ -28,7 +28,7 @@ Registered: ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}${lastclaim >
 }
 handler.help = ['profile [@user]']
 handler.tags = ['main']
-handler.command = /^profil?(e)$/i
+handler.command = /^profil(e?)$/i
 handler.register = true
 module.exports = handler
 
