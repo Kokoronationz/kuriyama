78bf07d04cb7d5df680203bb1f2d2e03ab05f535
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-23 07:32:40 +0700

    Add files via upload

---
+++ plugins/triggered.js
@@ -0,0 +1,40 @@
+//Thanks Nobuyaki:D
+
+const uploadImage = require('../lib/uploadImage') 
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text }) => {
+ try {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let triggered = `https://some-random-api.ml/canvas/triggered?avatar=${url}`
+  let stiker = await sticker(null, triggered, 'triggered', '@Kokoronationz')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+ } catch (e) {
+   m.reply('Conversion Failed')
+  }
+}
+handler.help = ['trigger (caption|reply media)']
+handler.tags = ['sticker']
+handler.command = /^trigger/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+handler.register = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
