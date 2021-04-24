9fa30c65f9d1dede4ee04ecfa33f6a09019a86f5
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-24 14:19:17 +0700

    Add files via upload

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -4,20 +4,15 @@ let handler = async (m, { conn, args }) => {
   let res = await fetch(global.API('xteam', '/dl/tiktok', {
     url: args[0]
   }, 'APIKEY'))
+  if (res.status !== 200) throw await res.text()
   let json = await res.json()
-  conn.sendFile(m.chat, json.result.url, 'tiktok.mp4', `
-_*Username:*_ @${json.result.username}
-`.trim(), m, false, {
-    thumbnail
-  })
+  if (!json.result) throw json
+  conn.sendFile(m.chat, json.result, 'tiktok.mp4', '', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^(tiktok(dl)?)$/i
 handler.register = true
-
-handler.fail = null
-handler.limit = true
 
 module.exports = handler
