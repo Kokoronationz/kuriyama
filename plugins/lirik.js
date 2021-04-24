382be5ccb958049b2d043c1d3e10c23d90be5ba0
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-25 06:41:43 +0700

    update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,15 +1,19 @@
-  const axios = require('axios')
+const axios = require('axios')
 
 let handler = async(m, { conn, text, usedPrefix }) => {
-
-    await m.reply('Searching...')
-    if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'lirik hanya rindu', m)
-
-    axios.get(`https://docs-jojo.herokuapp.com/api/lirik?q=` + text)
-        .then((res) => {
-            conn.reply(m.chat, res.data.result, m)
-        })
-        .catch()
+        let user = global.DATABASE._data.users[m.sender]
+    if (!text) return conn.reply(m.chat, '_Masukkan yang dicari_', m)
+    new Promise((resolve, reject) => {
+        axios.get(`https://ardi30.herokuapp.com/api/v1/lirik?l=` + encodeURIComponent(text))
+            .then((res) => {
+                      
+                const ardi = `*• Lirik Lagu :* ${res.data.data}`
+                conn.reply(m.chat, ardi, 'username',m)
+            })
+            .catch((err) => {
+                reject(err)
+            })
+    })
 }
 handler.help = ['lirik <judul lagu>']
 handler.tags = ['internet']
