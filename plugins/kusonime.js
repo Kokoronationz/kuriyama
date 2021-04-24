fe631f85af20adcc7d9d9936a856d8acba6f9bb7
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-25 06:05:23 +0700

    update kusonime.js

--- plugins/kusonime.js
+++ plugins/kusonime.js
@@ -4,7 +4,7 @@ let handler = async function (m, { text, isPrems, isOwner }) {
     if (!text) throw '_yg dicari apa_'
   let res = await fetch('https://ardhixsquerpants.herokuapp.com/api/kuso?q=' + encodeURIComponent(text))
 let json= await res.json()
-  const ardi =  `*judul* ${json.title}\n\n*info:* ${json.info}\n\n*sinopsis:* ${json.sinopsis}\n\n*link download:* ${json.link_dl}`
+  const ardi =  `*judul:* "${json.title}"\n\n*info:* ${json.info}\n\n*sinopsis:* ${json.sinopsis}\n*link download:* ${json.link_dl}`
      conn.sendFile(m.chat,json.thumb, 'image.jpg', ardi, m)
 }
 handler.help = ['kusonime <judul>']
