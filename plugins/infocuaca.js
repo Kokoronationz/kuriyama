4a23ce71e5ef1e7d7fb0a1cea2b1dde1c38acfb8
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-25 07:46:27 +0700

    update infocuaca.js

--- plugins/infocuaca.js
+++ plugins/infocuaca.js
@@ -4,11 +4,10 @@ let handler = async(m, { conn, text, usedPrefix }) => {
 
     await m.reply('Searching...')
     if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'infocuaca Bandung', m)
+    let ress = res.data.hasil
 
     axios.get(`https://ardi30.herokuapp.com/doc/cuaca?p=${text}`)
         .then((res) => {
-          let ress = res.data.hasil
-          
           let hasil = `*Kota:* ${text}\n*Koordinat:*\n  Lon: ${ress.koordinat.lon}\n  Lat: ${ress.koordinat.lat}\n*Suhu:* ${ress.suhu}\n*Angin:* ${ress.angin}\n*Kelembaban:* ${ress.kelembaban}\n*Cuaca:* ${res.cuaca}\n*Keterangan:* ${ress.keterangan}\n*Angin:* ${ress.angin}`
             conn.reply(m.chat, hasil, m)
         })
