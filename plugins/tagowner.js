f28019740d29bad7e56b0a6312c16c3faa0e80c1
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-28 03:36:08 +0700

    update tagowner.js

--- plugins/tagowner.js
+++ plugins/tagowner.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
     let name = ('62895361677059@s.whatsapp.net')
     let panggil = `
-    ${name.replace(/@.+/, '')}!!! Ada yang manggil nih:v
+    @${name.replace(/@.+/, '')}!!! Ada yang manggil nih:v
     `.trim()
     let mentionedJid = [name]
   conn.reply(m.chat, panggil, m, { contextInfo: { mentionedJid }})
