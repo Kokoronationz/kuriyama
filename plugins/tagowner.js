3d32777e52af16681b3201dbfeefb14c294abf62
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-28 02:58:49 +0700

    update tagowner.js

--- plugins/tagowner.js
+++ plugins/tagowner.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, text }) => {
-    let name = m.fromMe ? conn.user : conn.contacts['62895361677059@s.whatsapp.net']
+    let name = ['62895361677059@s.whatsapp.net']
 
   conn.reply(m.chat, `
-${name}!!! Ada Yang manggil nih:v
-`.trim(), m)
-    let mentionedJid = [name]
+@${name.replace(/@.+/, '')}!!! Ada Yang manggil nih:v
+`.trim(), m, { contextInfo: { mentionedJid }})
+
 }
 handler.customPrefix = /Hana-cwan/i
 handler.command = new RegExp
