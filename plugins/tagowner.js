b7b1f5727f6f45a7960abd18a3ed7444ad6d70b8
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-28 03:10:24 +0700

    update tagowner.js

--- plugins/tagowner.js
+++ plugins/tagowner.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
     //let member = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-    let name = ('62895361677059')
+    let name = ('62895361677059@s.whatsapp.net')
     let panggil = `
     @${name.replace(/@.+/, '')}!!! Ada Yang manggil nih:v
     `.trim()
