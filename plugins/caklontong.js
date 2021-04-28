77a2a398a230b20643a14e1c3aaa6d2cd5ff9b3f
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-28 19:38:00 +0700

    Add caklontong

---
+++ plugins/caklontong.js
@@ -0,0 +1,33 @@
+//by BochilGaming
+//https://github.com/BochilGaming/
+
+let fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 2500
+let handler  = async (m, { conn, usedPrefix }) => {
+    conn.caklontong = conn.caklontong ? conn.caklontong : {}
+    let id = m.chat
+    if (id in conn.caklontong) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
+        throw false
+    }
+    let res = await fetch('https://zahirr-web.herokuapp.com/api/kuis/caklontong?apikey=zahirgans')
+    let json = await res.json()
+    conn.caklontong[id] = [
+      await conn.reply(m.chat, `Soal: *${json.result.soal}*\nTimeout: *${(timeout / 1000).toFixed(2)} detik*\nKetik *${usedPrefix}hint caklontong* untuk hint\nBonus: +Rp${poin}`, m),
+      json, poin,
+      setTimeout(() => {
+        if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\n*${json.result.deskripsi}*`, conn.caklontong[id][0])
+        delete conn.caklontong[id]
+      }, timeout)
+    ]
+  }
+handler.help = ['caklontong']
+handler.tags = ['game']
+handler.command = /^caklontong/i
+handler.register = true
+
+handler.limit = true
+  
+module.exports = handler
