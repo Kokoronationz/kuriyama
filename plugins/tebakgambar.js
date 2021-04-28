70f7c7135c5ce8f73eaeee1de24649bf2cd0e9f6
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-28 19:42:19 +0700

    update tebakgambar.js

--- plugins/tebakgambar.js
+++ plugins/tebakgambar.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 1000
+let poin = 2500
 let handler  = async (m, { conn, usedPrefix }) => {
     conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
     let id = m.chat
