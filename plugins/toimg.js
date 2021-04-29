3826c235e53d8fc7283c1ef15107f43f2538830d
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-29 17:05:48 +0700

    update toimg.js

--- plugins/toimg.js
+++ plugins/toimg.js
@@ -15,7 +15,7 @@ let handler  = async (m, { conn }) => {
     im.stdin.write(sticker)
     im.stdin.end()
     im.on('exit', () => {
-      conn.sendMessage(m.chat, Buffer.concat(bufs), MessageType.image, caption: `*© Kuriyama-bot*`, {
+      conn.sendMessage(m.chat, Buffer.concat(bufs), MessageType.image, { caption: `*© Kuriyama-bot*`,
         quoted: m
       })
     })
