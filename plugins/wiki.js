8c7f767632fd0de755d81881ad1291cc72f38e32
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-04-30 02:49:57 +0700

    update wiki.js

--- plugins/wiki.js
+++ plugins/wiki.js
@@ -5,9 +5,9 @@ let handler = async(m, { conn, text, usedPrefix }) => {
     await m.reply('Searching...')
     if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'wikipedia bot', m)
 
-    axios.get(`https://videfikri.com/api/wiki/?query=` + text)
+    axios.get(`https://api.zeks.xyz/api/wiki?q=` + text + `&apikey=apivinz`)
         .then((res) => {
-            conn.reply(m.chat, res.data.result.isi_konten, m)
+            conn.reply(m.chat, res.data.result, m)
         })
         .catch()
 }
