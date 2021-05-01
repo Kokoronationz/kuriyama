d4c83c21f9f662bd874c76cee2ad0f36517d6f00
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-01 10:13:38 +0700

    update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -1,7 +1,6 @@
 let fetch = require('node-fetch')
 let googleIt = require('google-it')
 let handler = async (m, { conn, command, args }) => {
-  await m.reply('Searching...')
   let full = /f$/i.test(command)
   let text = args.join` `
   if (!text) return conn.reply(m.chat, 'Tidak ada teks untuk di cari', m)
@@ -10,8 +9,13 @@ let handler = async (m, { conn, command, args }) => {
   let msg = search.map(({ title, link, snippet}) => {
     return `*${title}*\n_${link}_\n_${snippet}_`
   }).join`\n\n`
-  let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
-  conn.sendFile(m.chat, ss, 'screenshot.png', url + '\n\n' + msg, m)
+  try {
+    let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
+    if (ss.includes('html')) throw ''
+    await conn.sendFile(m.chat, ss, 'screenshot.png', url + '\n\n' + msg, m)
+  } catch (e) {
+    m.reply(msg)
+  }
 }
 handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
 handler.tags = ['internet']
