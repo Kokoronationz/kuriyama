fda08f272fc8b6d21ba434fb3b1ec74f086d92b1
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-06 02:12:56 +0700

    update filmsearch.js

--- plugins/filmsearch.js
+++ plugins/filmsearch.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text }) => {
 	let [kata] = text.split `|`
 
     if (!kata) return conn.reply(m.chat, 'Masukan kata', m)
-  await m.reply('Searching...')
+  await m.reply(global.wait)
 	axios.get(`https://api.zeks.xyz/api/film/2?q=${kata}&apikey=apivinz`).then ((res) => {
 	 	let hasil = res.data.result.map(res=>`*➸ Judul: ${res.title}*\n*➸ Link: ${res.url}*\n*➸ Img: ${res.thumb}*`).join('\n\n')
 
