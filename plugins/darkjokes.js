718238df63ac7c1505b96a65413961db1dbc3265
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-06 05:09:40 +0700

    update darkjokes.js

--- plugins/darkjokes.js
+++ plugins/darkjokes.js
@@ -1,10 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  await m.reply(global.wait)
+  await m.reply('Fitur ini di nonaktifkan!')
+/*  await m.reply(global.wait)
   let url = await fetch('https://api.zeks.xyz/api/darkjokes?apikey=apivinz')
   let darkjokes = await url.json()
 
-  conn.sendFile(m.chat, darkjokes.result, 'filename', '_*Gelap euy:v*_')
+  conn.sendFile(m.chat, darkjokes.result, 'filename', '_*Gelap euy:v*_')*/
 }
 handler.help = ['darkjokes']
 handler.tags = ['fun']
