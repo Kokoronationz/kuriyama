200421244574bce5e939d6d823e7f6c60d9b7687
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-06 02:15:26 +0700

    update kbbi.js

--- plugins/kbbi.js
+++ plugins/kbbi.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text }) => {
 
     if (!text) return conn.reply(m.chat, 'Silahkan masukan kata yang akan diartikan', m)
 
-  await m.reply('Searching...')
+  await m.reply(global.wait)
 	axios.get(`https://mnazria.herokuapp.com/api/kbbi?search=${text}`).then ((res) => {
 	 	let hasil = `Arti kata *${text}* \n\n${res.data.result}`
 
