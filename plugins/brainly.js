2b34b6d0da33ee71d591b2b60e3c0431bb3cd673
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-06 02:05:02 +0700

    update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,6 +1,7 @@
 const brainly = require('brainly-scraper-v2')
 let handler = async function (m, { text }) {
   if (!text) throw 'Soalnya?'
+  await m.reply(global.wait)
   let res = await brainly(text)
   let answer = res.data.map((v, i) => `_*PERTANYAAN KE ${i + 1}*_\n${v.pertanyaan}\n${v.jawaban.map((v,i) => `*JAWABAN KE ${i + 1}*\n${v.text}`).join('\n')}`).join('\n\n•━━━━━━━━━━━━━━━•\n\n')
   m.reply(answer)
