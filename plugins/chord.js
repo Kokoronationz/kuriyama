3cf222a1baace190c6b97c887b0a3b52fe6b2af7
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-06 02:06:19 +0700

    update chord.js

--- plugins/chord.js
+++ plugins/chord.js
@@ -3,7 +3,7 @@
 let handler = async(m, { conn, text, usedPrefix }) => {
 
     if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'chord hanya rindu', m)
-    await m.reply('Searching...')
+    await m.reply(global.wait)
     axios.get(`https://docs-jojo.herokuapp.com/api/chord?q=` + text)
         .then((res) => {
           let hasil = `*• Chord Lagu ${text} :*\n${res.data.result}`
