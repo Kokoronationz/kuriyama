d5393327129154bab9089c62effc76c04679f301
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-05 20:45:55 +0700

    update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -2,18 +2,19 @@ let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text, participants }) => {
 	let user = global.DATABASE._data.users[m.sender]
-     let [l, r] = text.split `|`
+/*     let [l, r] = text.split `|`
          if (!l) return conn.reply(m.chat, 'Silahkan masukan nama anda\ncontoh: *#tagall Udin|Woy nimbrunggg lah*', m)
-    if (!r) return conn.reply(m.chat, 'Silahkan masukan pesan anda', m)
+    if (!r) return conn.reply(m.chat, 'Silahkan masukan pesan anda', m)*/
 
   let users = participants.map(u => u.jid)
-  conn.reply(m.chat, '*「 TAG ALL 」*\n\n*Nama:* ' + l +  '\n*Pesan:* ' + r +'\n\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, m, {
+//  conn.reply(m.chat, '*「 TAG ALL 」*\n\n*Nama:* ' + l +  '\n*Pesan:* ' + r +'\n\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, m, {
+  conn.reply(m.chat, '*「 TAG ALL 」*\n\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, m, {
     contextInfo: { mentionedJid: users }
  
   
 })
 }
-handler.help = ['tagall <nama>|<pesan>']
+handler.help = ['tagall']
 handler.tags = ['group']
 handler.command = /^tagall$/i
 handler.owner = false 
