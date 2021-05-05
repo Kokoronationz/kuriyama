0d8194f79ad0809e3da5903d4fcd7ce4c43e8773
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-06 02:05:54 +0700

    update cewek.js

--- plugins/cewek.js
+++ plugins/cewek.js
@@ -2,7 +2,7 @@ let imageToBase64 = require('image-to-base64');
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
 
-  await m.reply('Searching...')
+  await m.reply(global.wait)
     let items = ["ullzang girl", "cewe cantik", "hijab cantik", "korean girl", "remaja cantik", "cewek korea", "cewek jepang"];
     let cewe = items[Math.floor(Math.random() * items.length)];
     let url = "https://api.fdci.se/rep.php?gambar=" + cewe;
