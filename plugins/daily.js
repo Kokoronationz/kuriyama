9c97fdbd33b6178e9d7be4a9e9d7c05a5c05780d
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-07 22:20:30 +0700

    update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,10 +1,17 @@
 let handler = async (m, { conn }) => {
+  let LastClaim = global.DATABASE._data.users[m.sender].lastclaim
+let cdj = `${JaM(new Date - LastClaim}`
+let cdm = `${MeNit(new Date - LastClaim)}`
+let cds = `${DeTik(new Date - LastClaim)}`
+let cd1 = math.ceil(23 - cdj)
+let cd2 = Math.ceil(59 - cdm)
+let cd3 = Math.ceil(60 - cds)
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
     global.DATABASE._data.users[m.sender].uang += 5000
     global.DATABASE._data.users[m.sender].exp += 100
     m.reply('Selamat anda mendapatkan +Rp5000')
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
-  } else m.reply('Anda sudah mengklaim harian hari ini')
+  } else m.reply(`Anda sudah mengklaim harian hari ini\n\nTunggu ${cd1} Jam ${cd2} Menit ${cd3} Detik!`)
 }
 handler.help = ['daily', 'claim']
 handler.tags = ['xp']
@@ -24,3 +31,17 @@ handler.exp = 0
 
 module.exports = handler
 
+function JaM(ms) {
+  let m = isNaN(ms) ? '60' : Math.floor(ms / 360000) % 60
+  return [m].map(v => v.toString().padStart(2, 0) ).join(':')
+}
+
+function MeNit(ms) {
+  let m = isNaN(ms) ? '60' : Math.floor(ms / 60000) % 60
+  return [m].map(v => v.toString().padStart(2, 0) ).join(':')
+}
+
+function DeTik(ms) {
+  let s = isNaN(ms) ? '60' : Math.floor(ms / 1000) % 60
+  return [s].map(v => v.toString().padStart(2, 0) ).join(':')
+}
