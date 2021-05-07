9fdc1f1c2b9d1039e2509721473493245692761a
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-07 21:16:14 +0700

    update salam.js

--- plugins/salam.js
+++ plugins/salam.js
@@ -2,7 +2,7 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn ='src/waalaikumsalam.opus' //tergantung nama file
+let vn ='src/waalaikumsalam.opus'
 conn.sendFile(m.chat, vn, 'salam.opus', null, m, true, {
   type: 'audioMessage', 
   ptt: true
