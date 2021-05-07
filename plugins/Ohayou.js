e9bd7afa02d67a71d9f6e5e0a7a56497df9d0129
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-07 21:19:04 +0700

    Add files via upload

--- plugins/Ohayou.js
+++ plugins/Ohayou.js
@@ -2,11 +2,8 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn ='src/ohayou.opus' //tergantung nama file
-conn.sendFile(m.chat, vn, 'ohayou.opus', null, m, true, {
-  type: 'audioMessage', 
-  ptt: true
-})
+let vn ='src/ohayou.opus'
+conn.sendFile(m.chat, vn, 'audio.opus', null, m, true)
 }
 handler.customPrefix = /ohayou?/i
 handler.command = new RegExp
--- plugins/konnichiwa.js
+++ plugins/konnichiwa.js
@@ -2,11 +2,8 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn ='src/konnichiwa.opus' //tergantung nama file
-conn.sendFile(m.chat, vn, 'konnichiwa.opus', null, m, true, {
-  type: 'audioMessage', 
-  ptt: true
-})
+let vn ='src/konnichiwa.opus'
+conn.sendFile(m.chat, vn, 'audio.opus', null, m, true)
 }
 handler.customPrefix = /kon?nichiwa/i
 handler.command = new RegExp
