bcc73bf0851876f71ef510c4b26e75fc163175bd
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-08 09:09:47 +0700

    update how.js

--- plugins/how.js
+++ plugins/how.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
   let elu = m.sender
   let tag = elu + '@s.whatsapp.net'
-  if (!text) text = `@${tag.replace(/@.+/, '')}`
+  if (!text) text = `${tag.split`@`[0]}`
   conn.reply(m.chat, `
 ${command} *${text}*
 
