4207282216e8a9b1001e8ac3e82e656773e668f7
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-08 14:21:56 +0700

    update triggered.js

--- plugins/triggered.js
+++ plugins/triggered.js
@@ -5,17 +5,17 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text }) => {
   await m.reply(global.wait)
 try {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (!mime) throw 'Tidak ada foto'
-if (!/image|video/.test(mime)) throw Mime ${mime} tidak support
-let img = await q.download()
-let url = await uploadImage(img)
-let trigger = https://some-random-api.ml/canvas/triggered?avatar=${url}
-let stiker = await sticker(null, trigger, 'Triggered', '@kokoronation')
-conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-quoted: m
-})
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let trigger = `https://some-random-api.ml/canvas/triggered?avatar=${url}`
+  let stiker = await sticker(null, trigger, 'Triggered', '@kokoronation')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
 } catch (e) {
 m.reply('Conversion Failed')
 }
