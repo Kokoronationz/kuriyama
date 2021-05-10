eb97b654d7ab1ef88df8450ad21df095f1195ac2
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-10 11:25:04 +0700

    update groupinfo.js

--- plugins/groupinfo.js
+++ plugins/groupinfo.js
@@ -21,7 +21,6 @@ let handler = async (m, { conn }) => {
 Nama Group: ${res.subject}
 ID:	${res.id}
 Dibuat pada: ${formatDate(res.creation * 1000)}
-Jumlah Member: ${res.size}
 Deskripsi:
 ${res.desc}
 
