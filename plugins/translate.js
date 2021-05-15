657a134561091def7aacc024463636bea462fde3
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-15 19:30:14 +0700

    update translate.js

--- plugins/translate.js
+++ plugins/translate.js
@@ -1,11 +1,11 @@
 // ariffb - http:/wa.me/6283128734012
 const translate = require('translate-google-api')
-let handler = async (m, { text, usedPrefix }) => {
-    goblok = `contoh:\n${usedPrefix}tr lang teks\n${usedPrefix}tr id thankyou\n\nDaftar bahasa yang didukung: https://cloud.google.com/translate/docs/languages`
+let handler = async (m, { args, usedPrefix, command }) => {
+    er = `contoh: \n${usedPrefix + command} lang teks\n${usedPrefix + command} id your messages\n\nDaftar bahasa yang didukung: https://cloud.google.com/translate/docs/languages`
 
     let lang = 'en'
     let text = args.slice(1).join(' ')
-    if (!text) throw goblok
+    if (!text) throw er
     if (args[0].length === 2) lang = args[0]
     else text = args.join(' ')
     if (!text) text = lang
@@ -18,7 +18,7 @@ let handler = async (m, { text, usedPrefix }) => {
         m.reply(`To: ${lang}\n\nTerjemahan: ${result[0]}`)
         console.log(result[0])
     } catch (e) {
-        throw goblok
+        throw er
     }
 }
 handler.help = ['translate'].map(v => v + ' <lang> <teks>')
