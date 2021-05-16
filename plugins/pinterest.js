4b0404ea6b7d812d9fed6c8134d55b60af5e3cde
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-16 22:14:44 +0700

    update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -4,7 +4,7 @@ try {
     if (!text) return conn.reply(m.chat, '_Sil ahkan Masukan Teks!_', m)
     if (text.length > 10) return conn.reply(m.chat, '_Teks Terlalu Panjang! Maksimal 10 huruf!_', m)
     await m.reply(global.wait)
-    let link = 'https://lindow-api.herokuapp.com/api/pinterest?search=' + text;
+    let link = 'https://lindow-api.herokuapp.com/api/pinterest?search=' + text
     conn.sendFile(m.chat, link, 'pin.png', '*©Kuriyama-bot*', m)
   } catch (e) {
    m.reply('```Error```')
