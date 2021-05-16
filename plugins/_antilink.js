b6a9a6cbf1dd6240b8fe1969d628c4c1ff566000
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-16 21:17:26 +0700

    update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,13 +1,24 @@
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
-handler.before = function (m, { isAdmin, isBotAdmin }) {
+handler.before = function (m, { isAdmin, isBotAdmin, conn, participants }) {
+  const ambilAdminGrup = (participants) => {
+        admins = []
+        for (let i of participants) {
+            i.isAdmin ? admins.push(i.jid) : ''
+        }
+        return admins
+    }
+
+// 
+const adminGrup = ambilAdminGrup(participants)
+let daftar = adminGrup.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
   if (m.isBaileys && m.fromMe) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
-
+  if ( /izin min/.test(m.text) ) throw false
   if (chat.antiLink && isGroupLink) {
-    m.reply('Hapus!!\n\nLink Grup terdeteksi')
+    conn.reply(m.chat, `*「 Link terdeteksi! 」*\n\nsilahkan kak kalo mau kick :/\n${daftar}`, m, { contextInfo: { mentionedJid: adminGrup } })
     if (global.opts['restrict']) {
       if (isAdmin || !isBotAdmin) return true
       // this.groupRemove(m.chat, [m.sender])
