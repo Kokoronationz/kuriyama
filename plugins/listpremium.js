9046fa329cc2739307b25cf3b455cb65fca052f8
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-17 12:13:23 +0700

    update listpremium.js

--- plugins/listpremium.js
+++ plugins/listpremium.js
@@ -8,7 +8,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
   let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-conn.sendMessage(m.chat, `*「 List Premium 」*` + `\n` + prem.map(v => '❖ @' + v.replace(/@.+/, '')).join`\n`, MessageType.extendedText, { contextInfo: { mentionedJid: prem } })
+conn.sendMessage(m.chat, `*「 List Premium 」*` + `\n\n` + prem.map(v => '❖ @' + v.replace(/@.+/, '')).join`\n`, MessageType.extendedText, { contextInfo: { mentionedJid: prem } })
 }
 handler.help = ['listpremium']
 handler.tags = ['premium']
