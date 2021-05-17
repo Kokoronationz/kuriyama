962af6303a9bd6219fb3a08dfefeec08686afe02
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-17 21:34:42 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -285,7 +285,7 @@ module.exports = {
     }
   },
   async participantsUpdate({ jid, participants, action }) {
-    let chat = global.DATABASE._data.chats[jid]
+    let chat = global.DATABASE._data.chats[jid] || {}
     let text = ''
     switch (action) {
       case 'add':
