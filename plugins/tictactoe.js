4b9ba7c9a7992e415d03715b2ed4e76d4433177a
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-17 21:31:59 +0700

    update tictactoe.js

--- plugins/tictactoe.js
+++ plugins/tictactoe.js
@@ -1,9 +1,9 @@
 const TicTacToe = require("../lib/tictactoe")
 
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
     conn.game = conn.game ? conn.game : {}
     if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw 'Kamu masih didalam game'
-    let room = Object.values(conn.game).find(room => room.state == 'WAITING')
+    let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
     // m.reply('[WIP Feature]')
     if (room) {
         m.reply('Partner ditemukan!')
@@ -31,6 +31,7 @@ ${arr.slice(0, 3).join('')}
 ${arr.slice(3, 6).join('')}
 ${arr.slice(6).join('')}
 Menunggu @${room.game.currentTurn.split('@')[0]}
+Ketik *nyerah* untuk nyerah
 `.trim()
         if (room.x !== room.o) m.reply(str, room.x, {
             contextInfo: {
@@ -43,7 +44,6 @@ Menunggu @${room.game.currentTurn.split('@')[0]}
             }
         })
     } else {
-        m.reply('Menunggu partner...')
         room = {
             id: 'tictactoe-' + (+new Date),
             x: m.chat,
@@ -51,12 +51,15 @@ Menunggu @${room.game.currentTurn.split('@')[0]}
             game: new TicTacToe(m.sender, 'o'),
             state: 'WAITING'
         }
+        if (text) room.name = text
+        m.reply('Menunggu partner' + (text ? `mengetik command dibawah ini
+${usedPrefix}${command} ${text}` : ''))
         conn.game[room.id] = room
     }
 }
-handler.help = ['tictactoe']
+handler.help = ['tictactoe', 'ttt'].map(v => v + ' [custom room name]')
 handler.tags = ['game']
-handler.command = /^tictactoe$/
+handler.command = /^(tictactoe|t{3})$/
 handler.register = true
 
 module.exports = handler
