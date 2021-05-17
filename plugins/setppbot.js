c843cd665603e5d21e37ec864e52918fa9fb4ec4
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-17 11:58:12 +0700

    update setppbot.js

--- plugins/setppbot.js
+++ plugins/setppbot.js
@@ -1,43 +1,23 @@
-let { Presence } = require('@adiwajshing/baileys')
-let handler = async(m, { conn, text, args, bot, isOwner, command, usedPrefix }) => {
-    await m.reply(global.wait)
-    const type = Object.keys(m.message)[0]
-    const content = JSON.stringify(m.message)
-    const isMedia = (type === 'imageMessage' || type === 'videoMessage')
-    const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
-    const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
-         try {
-         if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
-         if (!isOwner) return m.reply('```Anda Siapa?```')
-         conn.updatePresence(m.chat, Presence.composing) 
-         //if (!isQuotedImage) return m.reply(`_Kirim gambar dengan caption ${usedPrefix}setppbot atau tag gambar yang sudah dikirim_`)
-         enmedia = isMedia || isQuotedImage ? JSON.parse(JSON.stringify(m).replace('quotedM','m')).message.extendedTextMessage.contextInfo : m
-         media = await conn.downloadAndSaveMediaMessage(enmedia)
-         await conn.updateProfilePicture(global.conn.user.jid, media)
-         m.reply('```Success Change Profil Picture```')
-  } else {
-        m.reply('_Reply Fotonya!_')
+/*
+   Silahkan Di Pakek
+   Tapi Bantu Rapihin :v
+   Buatan: Miaweers
+*/
+
+let handler = async (m, { conn, args }) => {
+    let bot = conn.user.jid // Bot
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image/.test(mime)) {
+      let img = await conn.downloadM(q)
+      if (!img) throw `Foto tidak ditemukan`
+     conn.updateProfilePicture (bot, img)
+    conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
         }
-	 } catch (e) {
-   m.reply('```Error```')
-   console.log(e)
- }
-}
-					
+    }
 handler.help = ['setppbot']
 handler.tags = ['owner']
-handler.command = /^(setppb?)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.limit = false
+handler.command = /^(setppbot)$/i
+handler.rowner = true
 
 module.exports = handler
