2d510fadba633925f551d45076d80165e48400e5
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-17 14:32:20 +0700

    update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -9,7 +9,6 @@ let handler = async function (m, { text, usedPrefix }) {
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
   if (!age) throw 'Umur tidak boleh kosong (Angka)'
-  if (!age < 14) throw 'Maaf, Anda tidak bisa Mendaftar'
   user.name = name
   user.age = parseInt(age)
   user.regTime = + new Date
