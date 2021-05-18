3d642a5ae3cd2b6f049ecbe59606d86aeb6e4af2
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-18 21:00:08 +0700

    update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
   
 let name = conn.getName(m.sender)
-let pesan = `Hai ${name}, aku disini:D\n\nAda yang bisa aku bantu?`)
+let pesan = `Hai ${name}, aku disini:D\n\nAda yang bisa aku bantu?`
 conn.reply(m.chat, pesan, m)
 }
 handler.customPrefix = /Bot/
