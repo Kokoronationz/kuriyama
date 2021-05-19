efa05d6d2a2a5d0ed62fe2eb5ba244e415956eeb
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-19 21:53:39 +0700

    update konbanwa.js

--- plugins/konbanwa.js
+++ plugins/konbanwa.js
@@ -2,10 +2,10 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './src/konbanwa.opus'
-conn.sendFile(m.chat, vn, 'konbanwa.opus', null, m, {
-  type: 'audioMessage',
-  ptt: true
+let vn = './src/sound/konbanwa.mp3'
+conn.sendFile(m.chat, vn, 'konbanwa.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /konban?wa/i
