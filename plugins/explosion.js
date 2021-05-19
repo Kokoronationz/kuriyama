a3bce4a1f3b5b52ea518e413fa92f7efbea8851d
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-19 21:51:54 +0700

    update explosion.js

--- plugins/explosion.js
+++ plugins/explosion.js
@@ -2,10 +2,10 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './src/explosion.opus'
-conn.sendFile(m.chat, vn, 'explosion.opus', null, m, {
-  type: 'audioMessage',
-  ptt: true
+let vn = './src/sound/explosion.mp3'
+conn.sendFile(m.chat, vn, 'explosion.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /bakuretsu|ledakin/i
