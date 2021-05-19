13efb3039a807d5c9b0a002b0a088ab1301ed853
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-19 22:38:07 +0700

    update salam.js

--- plugins/salam.js
+++ plugins/salam.js
@@ -2,7 +2,7 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './src/sound/waalaikumsalam.ogg'
+let vn = './src/sound/waalaikumsalam.mp3'
 conn.sendFile(m.chat, vn, 'waalaikumsalam.mp3', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
