5698d52a41886442e3b09249e7ca4d2c3488f610
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-19 20:31:29 +0700

    update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,7 +1,7 @@
 const { createHash } = require('crypto')
 let PhoneNumber = require('awesome-phonenumber')
 let levelling = require('../lib/levelling')
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, isPrems }) => {
   let pp = './src/avatar_contact.png'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
@@ -9,21 +9,31 @@ let handler = async (m, { conn }) => {
   } catch (e) {
 
   } finally {
+    let premium = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
     let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
     let { name, uang, limit, exp, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[who]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let sn = createHash('md5').update(who).digest('hex')
     let str = `
-Name: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
-Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-Link: https://wa.me/${who.split`@`[0]}${registered ? '\nAge: ' + age : ''}
-XP: TOTAL ${exp} (${exp - min} / ${xp}) [${max - exp} left to levelup]
-Saldo: Rp${uang}
-Level: ${level}
-Limit: ${limit}
-Registered: ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim) : ''}
-SN: ${sn}
+┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┇       *「 Profile 」*
+┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┃ *Name:* ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})
+┃ ${about ? '*About:* ' + about : ''}
+┃ *Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+┃ *Link:* https://wa.me/${who.split`@`[0]}
+┃ ${registered ? 'Age: ' + age : ''}
+┃ *XP:* TOTAL ${exp} (${exp - min} / ${xp}) 
+┃ [${max - exp} left to levelup]
+┃ *Saldo:* Rp${uang}
+┃ *Level:* ${level}
+┃ *Limit:* ${limit}
+┃ *Premium:* ${premium ? 'YES':'NO'}
+┃ *Registered:* ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}
+┃ ${lastclaim > 0 ? '\n*Last Claim:* ' + new Date(lastclaim) : ''}
+┃ *SN:* ${sn}
+┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
 `.trim()
     let mentionedJid = [who]
     conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
