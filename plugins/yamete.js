7161ea92836db61609eb333303ad31e9cc27b568
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-19 21:57:39 +0700

    update yamete.js

--- plugins/yamete.js
+++ plugins/yamete.js
@@ -2,8 +2,11 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn ='src/yamete.opus'
-conn.sendFile(m.chat, vn, 'yamete.opus', null, m, true)
+let vn ='src/sound/yamete.mp3'
+conn.sendFile(m.chat, vn, 'yamete.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+})
 }
 handler.customPrefix = /Yamete/
 handler.command = new RegExp
