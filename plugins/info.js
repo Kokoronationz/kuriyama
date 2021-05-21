6d228ac2203ab6823723e2bd096843a67c0acd21
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-21 13:35:49 +0700

    update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -43,25 +43,31 @@ let handler = async (m, { conn }) => {
   let speed = neww - old
   let txt = `
 Merespon dalam ${speed} millidetik
+
 💬 Status :
 - *${groups.length}* Group Chats
 - *${groupsIn.length}* Groups Joined
 - *${groups.length - groupsIn.length}* Groups Left
 - *${chats.length - groups.length}* Personal Chats
 - *${chats.length}* Total Chats
+
 📱 *Phone Info* :
 ${'```' + `
 🔋 Battery : ${conn.battery.value}% ${conn.battery.live ? '🔌 Charging...' : '⚡ Discharging'}
 ${util.format(conn.user.phone)}
 `.trim() + '```'}
+
 💻 *Server Info* :
 RAM: ${format(os.totalmem() - os.freemem())} / ${format(os.totalmem())}
+
 _NodeJS Memory Usage_
-${'```' + Object.keys(used).map(key => `${key} : ${format(used[key])}`).join('\n') + '```'}
-_Total CPU Usage_
+${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${format(used[key])}`).join('\n') + '```'}
+
+${cpus[0] ? `_Total CPU Usage_
 ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+
 _CPU Core(s) Usage (${cpus.length} Core CPU)_
-${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
 `.trim()
   m.reply(txt)
 }
