5817eb2c39e6e4afabee51cc8acd8f44c946bd8d
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-21 15:14:35 +0700

    Add files via upload

---
+++ plugins/smeme.js
@@ -0,0 +1,30 @@
+const uploadImage = require('../lib/uploadImage')
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text }) => {
+  await m.reply(global.wait)
+try {
+  let [text1, text2] = text.split('|')
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let meme = `https://docs-jojo.herokuapp.com/api/meme-gen?top=${text1}&bottom=${text2}&img=${url}`
+  let stiker = await sticker(null, meme, 'Sticker Meme', '@Kokoronationz')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+ } catch (e) {
+  }
+}
+
+handler.help = ['smeme <teks>|<teks>']
+handler.tags = ['sticker']
+handler.command = /^(smeme)$/i
+handler.limit = true
+handler.group = false
+handler.register = true
+module.exports = handler
