220b82b676905a5e23212cf15939782f5112dfdd
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-22 09:21:09 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -64,7 +64,7 @@ module.exports = {
           if (!'sDemote' in chat) chat.sDemote = ''
           if (!'delete' in chat) chat.delete = true
           if (!'antiLink' in chat) chat.antiLink = false
-          if (!'antiToxic' in chat) chat.antiToxic = true
+          if (!'antiToxic' in chat) chat.antiToxic = false
         } else global.DATABASE._data.chats[m.chat] = {
           isBanned: false,
           welcome: false,
