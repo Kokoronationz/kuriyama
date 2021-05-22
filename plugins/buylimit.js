13c4b189107f3c20eef7431aef4c87adc177105f
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-22 22:35:14 +0700

    update buylimit.js

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -1,4 +1,4 @@
-const uangperlimit = 500
+const uangperlimit = 1000
 let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buy/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.DATABASE._data.users[m.sender].uang / uangperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
