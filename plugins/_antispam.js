d29794b88a56463c3948fb7ee107d64ee842afa6
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-22 18:32:03 +0700

    update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -13,7 +13,7 @@ handler.all = async function (m) {
     } else try {
         this.spam[m.sender].spam += 1
         if (new Date - this.spam[m.sender].lastspam > 4000) {
-            if (this.spam[m.sender].spam > 6) {
+            if (this.spam[m.sender].spam > 4) {
                 this.spam[m.sender].spam = 0
                 this.spam[m.sender].lastspam = new Date * 1
                 //global.DATABASE._data.users[m.sender].banned = true
