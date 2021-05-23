c579007aafbe96e1b888933a01c622b0a6595938
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-23 09:16:47 +0700

    update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -5,35 +5,35 @@ let handler = async function (m, { text, usedPrefix }) {
   let rtotalreg = Object.values(global.DATABASE._data.users).filter(user => user.registered == true).length
   let user = global.DATABASE._data.users[m.sender]
   if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Format salah\n*${usedPrefix}daftar <nama>|<umur>*`
+  if (!Reg.test(text)) throw `Format salah\n\n*${usedPrefix}daftar <nama>|<umur>*`
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
   if (!age) throw 'Umur tidak boleh kosong (Angka)'
-  if (age < 14) throw 'Maaf, Anda tidak bisa mendaftar.'
+  if (age < 17) throw 'Maaf, Anda tidak bisa mendaftar.'
   user.name = name
   user.age = parseInt(age)
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-*「 Daftar Berhasil! 」*
-
 ┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┇       *「 DAFTAR BERHASIL 」*
+┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
 ┃ *Info*
 ┃
-┃ Nama: ${name}
-┃ Umur: ${age} tahun
-┃ Gift: Rp10000
-┃ Total Register: ${rtotalreg}
-┃ SN: ${sn}
+┃ *Nama:* ${name}
+┃ *Umur:* ${age} tahun
+┃ *Gift:* Rp10000
+┃ *Registered:* ${rtotalreg}
+┃ *SN:* 
+┃ ${sn}
 ┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-
 
  _Simpan Serial Number anda!_
 `.trim())
 global.DATABASE._data.users[m.sender].uang += 10000
 }
-handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>|<umur>')
+handler.help = ['daftar', 'register'].map(v => v + ' <nama>|<umur>')
 handler.tags = ['main']
 
 handler.command = /^(daftar|reg(ister)?)$/i
