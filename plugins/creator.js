1f1e6aa558cf321bec1c3993e27e3807cb52262e
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-23 23:21:59 +0700

    update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  //ini hanyalah contoh, jangan di uncomment_-
+  //Example this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
   this.sendContact(m.chat, '62895361677059', 'ココロナシ', m)
 }
 handler.help = ['owner', 'creator']
