6ebf471f5e47649c0340593ae5e8c7a5eaf53903
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-26 14:08:01 +0700

    update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,48 +1,7 @@
 let fs = require ('fs')
 let path = require('path')
 let levelling = require('../lib/levelling')
-let handler  = async (m, { conn, usedPrefix: _p, isPrems}) => {
-  try {
-    let package = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json')))
-    let kuriyama = './src/photo/kuriyama.png'
-    let { name, exp, uang, limit, level } = global.DATABASE.data.users[m.sender]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let kokoronationz = 'https://bit.ly/Kokoronationz'
-    let premium = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
-    //let name = conn.getName(m.sender)
-    let d = new Date
-    let locale = 'id'
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(((d * 1) + d.getTimezoneOffset()) / 84600000) % 5]
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    })
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    }).format(d)
-    let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    let _uptime = process.uptime() * 1000
-    let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
-    let uptime = clockString(_uptime)
-    let totalreg = Object.keys(global.DATABASE._data.users).length
-    let rtotalreg = Object.values(global.DATABASE._data.users).filter(user => user.registered == true).length
-    let tags = {
+let tags = {
       'main': 'Main',
       'xp': 'Exp & Limit',
       'sticker': 'Sticker',
@@ -72,29 +31,10 @@ let handler  = async (m, { conn, usedPrefix: _p, isPrems}) => {
       'info': 'Info',
       '': 'No Category',
     }
-    for (let plugin of Object.values(global.plugins))
-      if (plugin && 'tags' in plugin)
-        for (let tag of plugin.tags)
-          if (!tag in  tags) tags[tag] = tag
-    let help = Object.values(global.plugins).map(plugin => {
-      return {
-        help: plugin.help,
-        tags: plugin.tags,
-        prefix: 'customPrefix' in plugin,
-        limit: plugin.limit
-      }
-    })
-    let groups = {}
-    for (let tag in tags) {
-      groups[tag] = []
-      for (let menu of help)
-        if (menu.tags && menu.tags.includes(tag))
-          if (menu.help) groups[tag].push(menu)
-    }
-    conn.menu = conn.menu ? conn.menu : {}
-    let before = conn.menu.before || `
+const defaultMenu = {
+  before: `
 ┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇       *「 ${conn.user.name} 」*
+┇       *「 %me 」*
 ┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
 ┃
 ┃ ❖ Hai @${m.sender.split`@`[0]}!
@@ -127,17 +67,96 @@ let handler  = async (m, { conn, usedPrefix: _p, isPrems}) => {
 ┃ ❖ RC047
 ┃ ❖ Dan Kawan-kawan
 ┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-`
-    let header = conn.menu.header || '┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━\n┇       *「 %category 」*\n┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━'
-    let body   = conn.menu.body   || '┃ ❖  %cmd%islimit'
-    let footer = conn.menu.footer || '┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━\n'
-    let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
+`.trimStart(),
+    header: '┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━\n┇       *「 %category 」*\n┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━',
+    body  : '┃ ❖  %cmd%islimit',
+    footer: '┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━\n',
+    after  = : `
+*%npmname@^%version*
+${'```%npmdesc```'}
+`,
+}
+let handler  = async (m, { conn, usedPrefix: _p, isPrems}) => {
+  try {
+    let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
+    let kuriyama = './src/photo/kuriyama.png'
+    let { name, exp, uang, limit, level } = global.DATABASE.data.users[m.sender]
+    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let kokoronationz = 'https://bit.ly/Kokoronationz'
+    let premium = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+    //let name = conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'id'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.DATABASE._data.users).length
+    let rtotalreg = Object.values(global.DATABASE._data.users).filter(user => user.registered == true).length
+    for (let plugin of Object.values(global.plugins))
+      if (plugin && 'tags' in plugin)
+        for (let tag of plugin.tags)
+          if (!tag in  tags) tags[tag] = tag
+    let help = Object.values(global.plugins).map(plugin => {
+      return {
+        help: plugin.help,
+        tags: plugin.tags,
+        prefix: 'customPrefix' in plugin,
+        limit: plugin.limit
+      }
+    })
+    let groups = {}
+    for (let tag in tags) {
+      groups[tag] = []
+      for (let menu of help)
+        if (menu.tags && menu.tags.includes(tag))
+          if (menu.help) groups[tag].push(menu)
+    }
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body   = conn.menu.body   || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
     let _text  = before + '\n'
     for (let tag in groups) {
       _text += header.replace(/%category/g, tags[tag]) + '\n'
       for (let menu of groups[tag]) {
         for (let help of menu.help)
-          _text += body.replace(/%cmd/g, menu.prefix ? help : '%p' + help).replace(/%islimit/g, menu.limit ? ' (Limit)' : '')  + '\n'
+          _text += body.replace(/%cmd/g, menu.prefix ? help : '%p' + help).replace(/%islimit/g, () => {
+            let str = ''
+            if (menu.limit) str += ' (Limit)'
+            if (menu.premium) str += ' (Premium)'
+            return str + '\n'
+          })
       }
       _text += footer + '\n'
     }
@@ -146,6 +165,7 @@ let handler  = async (m, { conn, usedPrefix: _p, isPrems}) => {
     let replace = {
       '%': '%',
       p: _p, uptime, muptime,
+      me: conn.user.name,
       npmname: package.name,
       npmdesc: package.description,
       version: package.version,
