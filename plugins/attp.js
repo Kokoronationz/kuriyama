97a322810e6953c5d8675c454412c90d7372d405
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-28 17:02:38 +0700

    update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,14 +1,10 @@
-const { sticker3 } = require('../lib/sticker')
+const fetch = require('node-fetch')
+const FormData = require('form-data')
 const { MessageType } = require('@adiwajshing/baileys')
 
-let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Tidak ada teks?'
-  await m.reply(global.wait)
-  let stiker = await sticker3(null, global.API('xteam', '/attp', { file: '', text }))
-  if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-    quoted: m
-  })
-  throw stiker.toString()
+let handler  = async (m, { conn, text }) => {
+  if (text) conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, false, { asSticker: true })
+  else throw 'Uhm...Teksnya?'
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
