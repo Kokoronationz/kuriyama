c50dad1847d72e061f26573a178147955266d2a6
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-05-30 18:51:27 +0700

    update kisahnabi.js

--- plugins/kisahnabi.js
+++ plugins/kisahnabi.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text }) => {
 
   await m.reply(global.wait)
 	axios.get(`https://videfikri.com/api/religi/kisahnabi/?nabi=${text.toLowerCase()}`).then ((res) => {
-	 	let caption = `*Lahir Tahun:* ${res.data.result.tahun_kelahiran} SM\n*Tempat Lahir :* ${res.data.result.tempat_lahir}\n*Umur :* ${res.data.result.usia}*\n\n*Cerita:* ${res.data.result.description}`
+	 	let caption = `*Lahir Tahun:* ${res.data.result.tahun_kelahiran} SM\n*Tempat Lahir :* ${res.data.result.tempat_lahir}\n*Umur :* ${res.data.result.usia}\n\n*Cerita:* ${res.data.result.description}`
 	 	let pp = res.data.result.image
 if (pp) conn.sendFile(m.chat, pp, 'pp.jpg', caption, m)
   else m.reply(caption)
