2892981f94e7f2b81087af488972be9bd0ce5de6
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-01 21:02:34 +0700

    update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -7,13 +7,15 @@ let handler = async (m, { conn, text }) => {
   await m.reply(global.wait)
 try {
   let [text1, text2] = text.split('|')
+  if (!text1) text1 = ' '
+  if (!text2) text2 = ' '
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw 'Tidak ada foto'
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
   let img = await q.download()
   let url = await uploadImage(img)
-  let res = await fetch(`https://mnazria.herokuapp.com/api/create-meme?text-atas=` + encodeURIComponent(text1) + `&text-bawah=` + encodeURIComponent(text2) + `&background-url=${url}`
+  let res = await fetch(`https://mnazria.herokuapp.com/api/create-meme?text-atas=` + encodeURIComponent(text1) + `&text-bawah=` + encodeURIComponent(text2) + `&background-url=${url}`)
      let json = await res.json()
      let { gambar: meme } = json
   let stiker = await sticker(null, meme, 'Sticker Meme', '@Kokoronationz')
