9abaf7ecd5c68f7529066ac04b370805a1fa0e90
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-01 16:44:22 +0700

    update simsimi2.js

--- plugins/simsimi2.js
+++ plugins/simsimi2.js
@@ -2,12 +2,12 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   let res = await fetch(global.API('xteam', '/simsimi2', { kata: text }, 'APIKEY'))
   let json = await res.json()
-  if (json.status) m.reply(json.jawaban)
+  if (json.status) m.reply(json.result)
   else throw json
 }
-handler.help = ['simi2', 'simsimi2', 'simih2'].map(v => v + ' <teks>')
+handler.help = ['simii', 'simsimii'].map(v => v + ' <teks>')
 handler.tags = ['fun']
-handler.command = /^((sim)?simi2|simih2)$/i
+handler.command = /^((sim)?simii)$/i
 handler.group = false
 handler.register = true
 
