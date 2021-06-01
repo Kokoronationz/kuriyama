ee5f98a2dcaad4234d7d24e121e42f71b81b3b45
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-01 17:54:38 +0700

    update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,26 +1,12 @@
-const fetch = require('node-fetch')
-const FormData = require('form-data')
-const { MessageType } = require('@adiwajshing/baileys')
-
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Uhm...Teksnya?'
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
   await m.reply(global.wait)
-  conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text }), 'attp.webp', '', m, false, { asSticker: true })
+  conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
 handler.command = /^attp$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 handler.register = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 handler.limit = true
 
 module.exports = handler
