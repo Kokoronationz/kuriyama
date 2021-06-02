7ebf9fc4313dea4355d4f3387bbdbae410dd1bee
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-02 15:45:10 +0700

    update semoji.js

--- plugins/semoji.js
+++ plugins/semoji.js
@@ -1,15 +1,15 @@
 // Terimakasih kpd RC047 :v
 // Fitur By Xteams
+// Modified by Nurutomo (Update Xteam :v)
 
 const { sticker } = require('../lib/sticker')
-const { MessageType } = require('@adiwajshing/baileys')
-
+const fetch = require('node-fetch')
+const defaultType = 'whatsapp'
 let handler = async (m, { usedPrefix, conn, args, text }) => {
-  await m.reply(global.wait)
   let [tipe, emoji] = text.includes('|') ? text.split('|') : args
   if (tipe && !emoji) {
     emoji = tipe
-    tipe = 'whatsapp'
+    tipe = defaultType
   }
   if (!emoji) throw `Silahkan masukan emojinya\n\nMisal ${usedPrefix}semoji whatsapp 😎\n\nList Tipe:
 - whatsapp
@@ -17,27 +17,25 @@ let handler = async (m, { usedPrefix, conn, args, text }) => {
 - apple
 - google
 - microsoft`
-  let stiker = await sticker(null, global.API('xteam', '/sticker/emojitopng' + encodeURI(tipe.trim().toLowerCase()), { emo: emoji.trim() }, 'APIKEY'), global.packname, global.author)
-//   m.reply(`
-// Tipe: ${tipe.trim().toLowerCase()}
-// Emoji: ${emoji.trim()}
-// `.trim())
+  emoji = emoji.trim()
+  tipe = tipe.trim().toLowerCase()
+  await m.reply(global.wait)
+  let res = await fetch(global.API('xteam', '/sticker/emojitopng', { emo: emoji.trim() }, 'APIKEY'))
+  if (!res.ok) throw res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  if (!(tipe in json.result)) tipe = defaultType
+  let stiker = await sticker(null, json.result[tipe], global.packname, global.author, emoji)
+  //   m.reply(`
+  // Tipe: ${tipe}
+  // Emoji: ${emoji}
+  // `.trim())
   m.reply(stiker)
 }
 handler.help = ['semoji <tipe>|<emoji>']
 handler.tags = ['sticker']
 handler.command = /^s?emo(ji)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 handler.register = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 handler.limit = true
 
 module.exports = handler
