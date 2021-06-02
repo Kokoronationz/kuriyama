ea86a522dda32c177da5fdedcdfeb57e108834e3
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-03 00:39:26 +0700

    update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -83,8 +83,6 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     let kokoronationz = 'https://bit.ly/Kokoronationz'
     //let premium = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
     let tnbot = (await conn.getFile(await conn.getProfilePicture(m.fromMe))).data.toString('base64')
-    let tnyou = (await conn.getFile(await conn.getProfilePicture(m.sender))).data.toString('base64')
-    let tnl = tnyou ? tnyou : tnbot
     let { name, uang, exp, limit, level } = global.DATABASE.data.users[m.sender]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     //let name = conn.getName(m.sender)
@@ -186,12 +184,16 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
         fromMe: false 
       }, 
       message: { 
-        "imageMessage": { "mimetype": "image/jpeg", 
-        "caption": `${conn.user.name} Verified Bot`, 
-        "jpegThumbnail": tnbot
+        "imageMessage": { 
+          "mimetype": "image/jpeg", 
+          "caption": `${conn.user.name} Verified Bot`, 
+          "jpegThumbnail": tnbot
         } 
       }
-    }, m, { thumbnail: tnl, contextInfo: { mentionedJid: [m.sender]} } )
+    }, m, { 
+      thumbnail: tnbot, 
+      contextInfo: { 
+        mentionedJid: [m.sender]} } )
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
