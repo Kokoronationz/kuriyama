d279b585bbc696ea2c0c925d3292e7cb2d0e001c
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-03 21:09:40 +0700

    update caklontong_answer.js

--- plugins/caklontong_answer.js
+++ plugins/caklontong_answer.js
@@ -4,7 +4,7 @@
 let handler = m => m
 handler.before = async function (m) {
   let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint caklontong/i.test(m.quoted.text)) return
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/「 Cak Lontong 」/i.test(m.quoted.text)) return
   conn.caklontong = conn.caklontong ? conn.caklontong : {}
   if (!(id in conn.caklontong)) throw 'Soal itu telah berakhir!'
   if (m.quoted.id == conn.caklontong[id][0].id) {
