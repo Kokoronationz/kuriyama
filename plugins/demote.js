7f33180864c4432c285ff7d94fcebc968134be76
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-03 17:59:18 +0700

    update demote.js

--- plugins/demote.js
+++ plugins/demote.js
@@ -1,21 +1,15 @@
-let handler = async (m, { conn, args }) => {
-  let users = m.mentionedJid
-  conn.groupDemoteAdmin(m.chat, users)
+let handler = async (m, { conn, participants }) => {
+  let members = participants.filter(member => member.isAdmin).map(member => member.jid)
+  let users = m.mentionedJid.filter(user => members.includes(user))
+  for (let user of users) await conn.groupDemoteAdmin(m.chat, [user]).catch(console.log)
 }
-handler.help = ['demote','member','v'].map(v => v + ' @user')
+handler.help = ['demote','member'].map(v => v + ' @user')
 handler.tags = ['admin']
-handler.command = /^(demote|member|v)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
+handler.command = /^(demote|member)$/i
 handler.group = true
-handler.private = false
 handler.register = true
-
 handler.admin = true
 handler.botAdmin = true
-
-handler.fail = null
 handler.limit = true
 
 module.exports = handler
