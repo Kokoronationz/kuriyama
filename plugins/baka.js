b0e108c2f3a475ba750108f8b01f2e3e26df4a13
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-04 23:45:17 +0700

    update baka.js

--- plugins/baka.js
+++ plugins/baka.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Baka/i
+handler.customPrefix = /Baka/
 handler.command = new RegExp
 module.exports = handler
