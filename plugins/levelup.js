80e684a1d61878545db60b60c600474e77130477
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-04 18:53:30 +0700

    update levelup.js

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -9,11 +9,12 @@ Level *${user.level} (${user.exp - min}/${xp})*
 Kurang *${max - user.exp}* lagi!
 `.trim()
   }
-  user.level++
-  m.reply(`
+  let before = user.level * 1
+	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+	if (before !== user.level) m.reply(`
 Selamat, anda telah naik level!
-*${user.level - 1}* -> *${user.level}*
-  `.trim())
+*${before}* -> *${user.level}*
+	`.trim())
 }
 
 handler.help = ['levelup']
