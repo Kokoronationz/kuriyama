8c462ba7431c3840d68e18d47fcb5d2b935d75bc
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-05 00:29:37 +0700

    update tebakkata_answer.js

--- plugins/tebakkata_answer.js
+++ plugins/tebakkata_answer.js
@@ -7,12 +7,12 @@ handler.before = async function (m) {
   if (m.quoted.id == conn.tebakkata[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.tebakkata[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
+    if (' '+m.text.toUpperCase() == json.result.jawaban.toUpperCase()) {
       global.DATABASE._data.users[m.sender].uang += conn.tebakkata[id][2]
       m.reply(`*Benar!*\n+Rp${conn.tebakkata[id][2]}`)
       clearTimeout(conn.tebakkata[id][3])
       delete conn.tebakkata[id]
-    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
+    } else if (m.text.toUpperCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
     else m.reply(`*Salah!*`)
   }
   return !0
