79bf5705f062ecb1f021b7dce0017fa1f321fd09
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-05 00:40:20 +0700

    update siapakahaku.js

--- plugins/siapakahaku.js
+++ plugins/siapakahaku.js
@@ -20,6 +20,7 @@ ${json.result.level}
 Soal: "${json.result.soal}"
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik *${usedPrefix}sahint* untuk bantuan
 Bonus: Rp${poin}
     `.trim()
     conn.siapakahaku[id] = [
