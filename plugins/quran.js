4157ebd3b44209527f300954c01b8fdafd3ec87b
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-06 13:13:28 +0700

    update quran.js

--- plugins/quran.js
+++ plugins/quran.js
@@ -7,30 +7,29 @@ let [text1, text2] = text.split('|')
 
   await m.reply(global.wait)
 	axios.get(`https://islamic-api-indonesia.herokuapp.com/api/data/quran?surah=${text1}&ayat=${text2}`).then ((res) => {
-	 	let res = res.data.result.data
 	 	let caption = `
 *「 Quran 」*
 
-${res.surah.preBismillah.text.arab}
-${res.surah.preBismillah.translation.en}
+${res.data.result.data.surah.preBismillah.text.arab}
+${res.data.result.data.surah.preBismillah.translation.en}
 *Surah dan ayat ditemukan!*
 *Keterangan surah*
-*Nama surah :* ${res.surah.name.short} (${res.surah.transliteration.id}) (${res.surah.name.translation})
-*Surah ke-:* ${res.surah.number}
-*Jumlah ayat:* ${res.surah.numberOfVerses}
-*Turun di:* ${res.surah.revelation.id}
-*Tafsir:* ${res.surah.tafsir.id}
+*Nama surah :* ${res.data.result.data.surah.name.short} (${res.data.result.data.surah.transliteration.id}) (${res.data.result.data.surah.name.translation})
+*Surah ke-:* ${res.data.result.data.surah.number}
+*Jumlah ayat:* ${res.data.result.data.surah.numberOfVerses}
+*Turun di:* ${res.data.result.data.surah.revelation.id}
+*Tafsir:* ${res.data.result.data.surah.tafsir.id}
 
 *Keterangan Ayat*
-*Ayat ke-:* ${res.number.inSurah}
+*Ayat ke-:* ${res.data.result.data.number.inSurah}
 
-${res.text.arab}
-${res.text.transliteration.en}
+${res.data.result.data.text.arab}
+${res.data.result.data.text.transliteration.en}
 
-*Artinya :* ${res.translation.id}
-*Tafsir :* ${res.tafsir.id.short}
+*Artinya :* ${res.data.result.data.translation.id}
+*Tafsir :* ${res.data.result.data.tafsir.id.short}
 `.trim()
-	 	let pp = res.audio.primary
+	 	let pp = res.data.result.data.audio.primary
  conn.reply(m.chat, caption, m)
  conn.sendFile(m.chat, pp, 'pp.mp3', '', m)
 })
