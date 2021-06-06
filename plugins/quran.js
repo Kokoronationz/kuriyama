76a95530acc40efd4004fade40c73574c93fe0e1
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-06 12:26:16 +0700

    update quran.js

--- plugins/quran.js
+++ plugins/quran.js
@@ -7,7 +7,28 @@ let [text1, text2] = text.split('|')
 
   await m.reply(global.wait)
 	axios.get(`https://islamic-api-indonesia.herokuapp.com/api/data/quran?surah=${text1}&ayat=${text2}`).then ((res) => {
-	 	let caption = `*「 Quran 」*\n\n ${res.data.result.data.surah.preBismillah.text.arab}\n ${res.data.result.data.surah.preBismillah.translation.id}\n\n *Surah dan ayat ditemukan!* \n\n *Keterangan surah* \n*Nama surah :* ${res.data.result.data.surah.name.short} (${res.data.result.data.surah.transliteration.id}) (${res.data.result.data.surah.name.translation})\n*Surah ke-:* ${res.data.result.data.surah.number}\n*Jumlah ayat:* ${res.data.result.data.surah.numberOfVerses}\n*Turun di:* ${res.data.result.data.surah.revelation.id}\n*Tafsir:* ${res.data.result.data.surah.tafsir.id}\n\n *Keterangan Ayat* \n*Ayat ke-:* ${res.data.result.data.number.inSurah}\n\n ${res.data.result.data.text.arab}\n ${res.data.result.data.text.transliteration.en}\n\n *Artinya :* ${res.data.result.data.translation.id}\n *Tafsir :* ${res.data.result.data.tafsir.id.short}`
+	 	let caption = `
+*「 Quran 」*
+
+${res.data.result.data.surah.preBismillah.text.arab}
+${res.data.result.data.surah.preBismillah.translation.id}
+*Surah dan ayat ditemukan!*
+*Keterangan surah*
+*Nama surah :* ${res.data.result.data.surah.name.short} (${res.data.result.data.surah.transliteration.id}) (${res.data.result.data.surah.name.translation})
+*Surah ke-:* ${res.data.result.data.surah.number}
+*Jumlah ayat:* ${res.data.result.data.surah.numberOfVerses}
+*Turun di:* ${res.data.result.data.surah.revelation.id}
+*Tafsir:* ${res.data.result.data.surah.tafsir.id}
+
+*Keterangan Ayat*
+*Ayat ke-:* ${res.data.result.data.number.inSurah}
+
+${res.data.result.data.text.arab}
+${res.data.result.data.text.transliteration.en}
+
+*Artinya :* ${res.data.result.data.translation.id}
+*Tafsir :* ${res.data.result.data.tafsir.id.short}
+`.trim()
 	 	let pp = res.data.result.data.audio.primary
  conn.reply(m.chat, caption, m)
  conn.sendFile(m.chat, pp, 'pp.mp3', '', m)
