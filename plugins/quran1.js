dc6da4453b49661821ae47ff3152ef24d2f3c972
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-06 12:07:17 +0700

    update quran1.js

--- plugins/quran1.js
+++ plugins/quran1.js
@@ -9,7 +9,7 @@ let [text1, text2] = text.split('|')
 	axios.get(`https://islamic-api-indonesia.herokuapp.com/api/data/quran?surah=${text1}&ayat=${text2}`).then ((res) => {
 	 	let caption = `*「 Quran 」*\n\n ${res.data.result.data.preBismillah.text.arab}`
 	 	let pp = res.data.result.data.audio.primary
- await conn.reply(m.chat, caption, m)
+ conn.reply(m.chat, caption, m)
  conn.sendFile(m.chat, pp, 'pp.mp3', '', m)
 })
 }
