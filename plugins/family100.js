6ea70343a7d1c3372aa431cc07445b87001724b7
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-07 13:42:06 +0700

    update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -1,5 +1,5 @@
 let fetch = require('node-fetch')
-let winScore = 1000
+let winScore = 500
 async function handler(m) {
     this.game = this.game ? this.game : {}
     let id = 'family100_' + m.chat
