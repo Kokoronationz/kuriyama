81387f4b07f1fe8bbc1d510d3415b7419be22387
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-08 23:44:01 +0700

    update kisahnabi.js

--- plugins/kisahnabi.js
+++ plugins/kisahnabi.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text }) => {
 
   await m.reply(global.wait)
 	axios.get(`https://islamic-api-indonesia.herokuapp.com/api/data/kisahnabi?nabi=${text.toLowerCase()}`).then ((res) => {
-	 	let caption = `*「 Kisah Nabi 」*\n\n*Nama:* ${res.data.result.nabi.nabi}\n*Lahir Tahun:* ${res.data.result.nabi.lahir} SM\n*Tempat Lahir:* ${res.data.result.nabi.tempat}\n*Umur:* ${res.data.result.nabi.umur}\n\n*Cerita:* ${res.data.result.nabi.kisah}`
+	 	let caption = `*「 Kisah Nabi 」*\n\n*Nama:* ${res.data.result.nabi.nabi}\n*Lahir Tahun:* ${res.data.result.nabi.lahir} SM\n*Tempat Lahir:* ${res.data.result.nabi.tempat}\n*Umur:* ${res.data.result.nabi.umur} Tahun\n\n*Cerita:* ${res.data.result.nabi.kisah}`
 	 	let pp = res.data.result.nabi.image
 if (pp) conn.sendFile(m.chat, pp, 'pp.jpg', caption, m)
   else m.reply(caption)
