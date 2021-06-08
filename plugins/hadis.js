47aeafc2d4a78829dc7c70fb07f98abdad2c9cf0
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-08 23:31:18 +0700

    update hadis.js

--- plugins/hadis.js
+++ plugins/hadis.js
@@ -1,13 +1,12 @@
 const axios = require('axios')
 
-let handler = async(m, { conn, args, text }) => {
+let handler = async(m, { conn, text }) => {
 
-    let nomor = text
-    if (args.length == 0) throw `Masukkan nama Hadis\n\n❖ Abu Daud\n❖ Ahmad\n❖ Bukhari\n❖ Darimi\n❖ Ibnu Majah\n❖ Nasai\n❖ Malik\n❖ Muslim`
-    if (args[0] == 'abu daud' || args[0] == 'ahmad' || args[0] == 'bukhari' || args[0] == 'Darimi'|| args[0] == 'ibnu majah'|| args[0] == 'nasai'|| args[0] == 'malik'|| args[0] == 'muslim')
+    let [hadis, nomor] = text.split('|')
+    if (!hadis) throw `Masukkan nama Hadis\n\n❖ Abu Daud\n❖ Ahmad\n❖ Bukhari\n❖ Darimi\n❖ Ibnu Majah\n❖ Nasai\n❖ Malik\n❖ Muslim`
     await m.reply(global.wait)
     new Promise((resolve, reject) => {
-        axios.get(`https://islamic-api-indonesia.herokuapp.com/api/data/hadith?kitab=${args[0]}&nomor=${nomor}`)
+        axios.get(`https://islamic-api-indonesia.herokuapp.com/api/data/hadith?kitab=${hadis.toLowerCase()}&nomor=${nomor}`)
             .then((res) => {
 
                 let caption = `*「 Hadis 」*\n\n${res.data.result.data.contents.arab}\n"${res.data.result.data.contents.id}"\n\n${res.data.result.data.name} ${res.data.result.data.contents.number}.`
