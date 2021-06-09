ea19cd95c3e4b5661f7206537023e2924264885f
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-06-09 13:16:08 +0700

    update ytComment.js

--- plugins/ytComment.js
+++ plugins/ytComment.js
@@ -1,25 +1,13 @@
-const uploadImage = require('../lib/uploadImage')
-const { MessageType } = require('@adiwajshing/baileys')
-
 let handler = async (m, { conn, text }) => {
-
-try {
-  let [text1, text2] = text.split('|')
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'No photo'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} not supported`
-  await m.reply(global.wait)
-  let img = await q.download()
-  let url = await uploadImage(img)
-  let foto = `https://some-random-api.ml/canvas/youtube-comment?avatar=${url}&comment=${text2}&username=${text1}`
-  conn.sendFile(m.chat, foto, 'ytc.jpg', '*©Kuriyama-Bot*', m)
-} catch (e) {
-  m.reply('Conversion Failed')
-  }
+  if (!text) throw 'No Text'
+  conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
+    avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
+    comment: text,
+    username: conn.getName(m.sender)
+  }), 'yt-comment.png', '*©Kuriyama-Bot*', m)
 }
 
-handler.help = ['ytcomment <username>|<comment>']
+handler.help = ['ytcomment <comment>']
 handler.tags = ['creator']
 handler.command = /^(ytc(omment)?)$/i
 handler.limit = true
